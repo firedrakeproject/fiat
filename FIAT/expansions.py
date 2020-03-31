08fbb554c827dfdf83c893151dd2e03837f58582
Reuben Hill <ReubenHill@users.noreply.github.com>
2020-03-31 12:29:45 +0100

    correct tabulation for PointExpansionSet

--- FIAT/expansions.py
+++ FIAT/expansions.py
@@ -116,11 +116,6 @@ class PointExpansionSet(object):
             raise Exception("Must have a point")
         self.ref_el = ref_el
         self.base_ref_el = reference_element.Point()
-        v1 = ref_el.get_vertices()
-        v2 = self.base_ref_el.get_vertices()
-        self.A, self.b = reference_element.make_affine_mapping(v1, v2)
-        self.mapping = lambda x: numpy.dot(self.A, x) + self.b
-        self.scale = numpy.sqrt(numpy.linalg.det(self.A))
 
     def get_num_members(self, n):
         return 1
@@ -128,15 +123,8 @@ class PointExpansionSet(object):
     def tabulate(self, n, pts):
         """Returns a numpy array A[i,j] = phi_i(pts[j])"""
         if len(pts) > 0:
-            raise Exception("Can't tabulate on point reference elements")
-            # ref_pts = numpy.array([self.mapping(pt) for pt in pts])
-            # psitilde_as = jacobi.eval_jacobi_batch(0, 0, n, ref_pts)
-
-            # results = numpy.zeros((n + 1, len(pts)), type(pts[0][0]))
-            # for k in range(n + 1):
-            #     results[k, :] = psitilde_as[k, :] * math.sqrt(k + 0.5)
-
-            # return results
+            results = numpy.ones((n + 1, len(pts)), type(pts[0]))
+            return results
         else:
             return []
 
