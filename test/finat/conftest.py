2abb505c9e1df34e5dc21bc04d6bf4324b648b72
Pablo Brubeck <brubeck@protonmail.com>
2024-12-10 12:16:42 +0000

    Extend MyMapping to 3D

--- test/finat/conftest.py
+++ test/finat/conftest.py
@@ -26,27 +26,36 @@ class MyMapping(PhysicalGeometry):
         return gem.Literal(self.A)
 
     def normalized_reference_edge_tangents(self):
-        return gem.Literal(np.asarray([self.ref_cell.compute_normalized_edge_tangent(i) for i in range(3)]))
+        top = self.ref_cell.get_topology()
+        return gem.Literal(
+            np.asarray([self.ref_cell.compute_normalized_edge_tangent(i)
+                        for i in sorted(top[1])]))
 
     def reference_normals(self):
+        sd = self.ref_cell.get_spatial_dimension()
+        top = self.ref_cell.get_topology()
         return gem.Literal(
             np.asarray([self.ref_cell.compute_normal(i)
-                        for i in range(3)]))
+                        for i in sorted(top[sd-1])]))
 
     def physical_normals(self):
+        sd = self.phys_cell.get_spatial_dimension()
+        top = self.phys_cell.get_topology()
         return gem.Literal(
             np.asarray([self.phys_cell.compute_normal(i)
-                        for i in range(3)]))
+                        for i in sorted(top[sd-1])]))
 
     def physical_tangents(self):
+        top = self.phys_cell.get_topology()
         return gem.Literal(
             np.asarray([self.phys_cell.compute_normalized_edge_tangent(i)
-                        for i in range(3)]))
+                        for i in sorted(top[1])]))
 
     def physical_edge_lengths(self):
+        top = self.phys_cell.get_topology()
         return gem.Literal(
             np.asarray([self.phys_cell.volume_of_subcomplex(1, i)
-                        for i in range(3)]))
+                        for i in sorted(top[1])]))
 
     def physical_points(self, ps, entity=None):
         prefs = ps.points
@@ -61,10 +70,28 @@ class ScaledMapping(MyMapping):
 
     def cell_size(self):
         # Firedrake interprets this as 2x the circumradius
-        cs = (np.prod([self.phys_cell.volume_of_subcomplex(1, i)
-                       for i in range(3)])
-              / 2.0 / self.phys_cell.volume())
-        return np.asarray([cs for _ in range(3)])
+        sd = self.phys_cell.get_spatial_dimension()
+        top = self.phys_cell.get_topology()
+        vol = self.phys_cell.volume()
+        edges = [self.phys_cell.volume_of_subcomplex(1, i)
+                 for i in sorted(top[1])]
+
+        if sd == 1:
+            cs = vol
+        elif sd == 2:
+            cs = np.prod(edges) / (2 * vol)
+        elif sd == 3:
+            edge_pairs = [edges[i] * edges[j] for i in top[1] for j in top[1]
+                          if len(set(top[1][i] + top[1][j])) == len(top[0])]
+            cs = 1.0 / (12 * vol)
+            for k in range(4):
+                s = [1]*len(edge_pairs)
+                if k > 0:
+                    s[k-1] = -1
+                cs *= np.dot(s, edge_pairs) ** 0.5
+        else:
+            raise NotImplementedError(f"Cell size not implemented in {sd} dimensions")
+        return np.asarray([cs for _ in sorted(top[0])])
 
 
 def scaled_simplex(dim, scale):
