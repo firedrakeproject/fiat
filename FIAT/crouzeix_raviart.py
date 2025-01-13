f74ba4c2b403dbd58b0e6be517eec177fb557485
Pablo Brubeck <brubeck@protonmail.com>
2025-01-13 17:41:16 +0000

    CR: correct order of convergence for integral variant (only on triangles)

--- FIAT/crouzeix_raviart.py
+++ FIAT/crouzeix_raviart.py
@@ -14,7 +14,6 @@ from FIAT import finite_element, polynomial_set, dual_set, functional
 from FIAT.check_format_variant import check_format_variant
 from FIAT.quadrature_schemes import create_quadrature
 from FIAT.quadrature import FacetQuadratureRule
-from FIAT.hierarchical import make_dual_bubbles
 from FIAT.reference_element import make_lattice
 
 
@@ -37,13 +36,16 @@ class CrouzeixRaviartDualSet(dual_set.DualSet):
                     continue
                 facet = ref_el.construct_subelement(dim)
                 if dim == 0:
-                    Q_facet = create_quadrature(facet, degree-1 + interpolant_deg)
-                    Phis = numpy.empty((0, len(Q_facet.pts)))
+                    Q_facet = create_quadrature(facet, degree + interpolant_deg-1)
+                    Phis = numpy.ones((1, len(Q_facet.pts)))
                 else:
-                    Q_facet, Phis = make_dual_bubbles(facet, degree, interpolant_deg=interpolant_deg)
-                if dim == sd-1:
-                    # Include constant moment on codim 1 facets
-                    Phis = numpy.vstack((numpy.ones((1, Phis.shape[1])), Phis))
+                    k = degree - 1 if dim == sd-1 else degree - (1+dim)
+                    if k < 0:
+                        continue
+                    Q_facet = create_quadrature(facet, k + interpolant_deg)
+                    poly_set = polynomial_set.ONPolynomialSet(facet, k)
+                    Phis = poly_set.tabulate(Q_facet.get_points())[(0,) * dim]
+
                 for i in sorted(top[dim]):
                     cur = len(nodes)
                     Q = FacetQuadratureRule(ref_el, dim, i, Q_facet)
@@ -63,6 +65,7 @@ class CrouzeixRaviartDualSet(dual_set.DualSet):
                         pts = make_lattice(verts, degree-1, variant="gl")
                     else:
                         pts = ref_el.make_points(dim, i, degree, variant="gll")
+
                     nodes.extend(functional.PointEvaluation(ref_el, x) for x in pts)
                     entity_ids[dim][i].extend(range(cur, len(nodes)))
 
