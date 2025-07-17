41ba8fbf98dc73dbc6cf2bc5c43e64570ac1b1af
India Marsden <imm1117@ic.ac.uk>
2025-07-17 13:14:50 +0100

    lint

--- gem/gem_to_gpu.py
+++ gem/gem_to_gpu.py
@@ -2,8 +2,9 @@ from gem import gem
 import numpy as np
 from functools import singledispatch
 
+
 def construct_einsum_str(expr, index):
-    idx_dict = {idx : chr(65 + i) for i, idx in enumerate(set(sum(index+[expr.free_indices], tuple())))}
+    idx_dict = {idx: chr(65 + i) for i, idx in enumerate(set(sum(index + [expr.free_indices], tuple())))}
     idx_str = ""
     for idx in index:
         for sub in idx:
@@ -14,11 +15,11 @@ def construct_einsum_str(expr, index):
     idx_str += "".join([idx_dict[free] for free in expr.free_indices])
     return idx_str
 
+
 def to_cupy(assignments):
 
-    func_decl = lambda *args: [f"def cupy_kernel({",".join(args)}):"]
-    args = {} 
-    declare = {"counter" : 0}
+    args = {}
+    declare = {"counter": 0}
 
     @singledispatch
     def recurse(expr):
@@ -49,7 +50,7 @@ def to_cupy(assignments):
 
     @recurse.register(gem.Sum)
     def recurse_sum(expr):
-        summands =  [recurse(e) for e in expr.children] 
+        summands = [recurse(e) for e in expr.children]
         commands = [s[0] for s in summands]
         index = [s[1] for s in summands]
         assert len(set(index)) == 1
@@ -57,7 +58,7 @@ def to_cupy(assignments):
 
     @recurse.register(gem.Division)
     def recurse_div(expr):
-        summands =  [recurse(e) for e in expr.children] 
+        summands = [recurse(e) for e in expr.children]
         commands = [s[0] for s in summands]
         index = [s[1] for s in summands]
         assert len(set(index)) == 1
@@ -65,7 +66,7 @@ def to_cupy(assignments):
 
     @recurse.register(gem.MathFunction)
     def recurse_fn(expr):
-        chld, idx =  recurse(expr.children[0]) 
+        chld, idx = recurse(expr.children[0])
         name = expr.name
         if name != "abs":
             name = "cp." + name
@@ -73,16 +74,17 @@ def to_cupy(assignments):
 
     @recurse.register(gem.MaxValue)
     def recurse_max(expr):
-        chld, idx =  recurse(expr.children[0]) 
+        chld, idx = recurse(expr.children[0])
         return f"cp.max({chld})", idx
 
     @recurse.register(gem.ListTensor)
     def recurse_list_tensor(expr):
-        str_array = "cp." + repr(np.empty_like(expr.array)).replace('None', "{}").replace("object", "cp.float64")
+        str_array = repr(np.empty_like(expr.array)).replace('None', "{}")
+        str_array = "cp." + str_array.replace("object", "cp.float64")
         chld_list = []
         idx_list = []
         for chld_expr in expr.array.flatten():
-            chld, idx =  recurse(chld_expr) 
+            chld, idx = recurse(chld_expr)
             chld_list += [chld]
             idx_list += [idx]
         assert len(set(idx_list)) == 1
@@ -90,7 +92,7 @@ def to_cupy(assignments):
 
     @recurse.register(gem.Indexed)
     def recurse_indexed(expr):
-        chld, idx =  recurse(expr.children[0]) 
+        chld, idx = recurse(expr.children[0])
         chld += "["
         for i in expr.multiindex:
             if isinstance(i, gem.Index):
@@ -98,12 +100,12 @@ def to_cupy(assignments):
             else:
                 chld += f"{i},"
         chld = chld[:-1] + "]"
-        return chld, expr.index_ordering() + idx 
-    
+        return chld, expr.index_ordering() + idx
+
     @recurse.register(gem.FlexiblyIndexed)
     def recurse_findexed(expr):
         # TODO this doesn't encapsulate the detail dim2idx
-        chld, idx =  recurse(expr.children[0]) 
+        chld, idx = recurse(expr.children[0])
         chld += "["
         for (off, var) in expr.dim2idxs:
             if len(var) == 0:
@@ -111,40 +113,42 @@ def to_cupy(assignments):
             else:
                 for (i, stride) in var:
                     if isinstance(i, gem.Index):
-                        #chld += f"{off},:,"
-                        chld += f":,"
+                        # chld += f"{off},:,"
+                        chld += ":,"
                     else:
                         breakpoint()
         chld = chld[:-1] + "]"
-        return chld , expr.index_ordering()
+        return chld, expr.index_ordering()
 
     @recurse.register(gem.Variable)
     def recurse_variable(expr):
         args[expr.name] = 1
-        return expr.name, tuple() 
+        return expr.name, tuple()
 
     @recurse.register(gem.Literal)
     def recurse_literal(expr):
         if expr not in declare.keys():
             declare[expr] = (f"t{declare["counter"]}", expr.array)
-            declare["counter"] += 1 
+            declare["counter"] += 1
         return declare[expr][0], tuple()
-    
+
+    def func_decl(*args):
+        return [f"def cupy_kernel({",".join(args)}):"]
+
     strs = []
     for var, expr in assignments:
         e, e_idx = recurse(expr)
         v, v_idx = recurse(var)
         assert v_idx == e_idx
         strs += [f"\t{v}=cp.array({e})"]
-        
-    
+
     temp_vars = []
     for key, val in declare.items():
-       if key != "counter" and val[0][0] == "t":
-            temp_vars += [f"\t{val[0]}=cp.{repr(val[1])}"] 
-       elif key != "counter":
-            #temp_vars += ["\tbreakpoint()"]
-            temp_vars += [f"\t{val[0]}={repr(val[1])[1:-1]}"] 
+        if key != "counter" and val[0][0] == "t":
+            temp_vars += [f"\t{val[0]}=cp.{repr(val[1])}"]
+        elif key != "counter":
+            # temp_vars += ["\tbreakpoint()"]
+            temp_vars += [f"\t{val[0]}={repr(val[1])[1:-1]}"]
     res = "\n".join(func_decl(*list(args.keys())) + temp_vars + strs)
 
     return res
