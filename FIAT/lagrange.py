3634bc59d93a3b038ef4dd3215778526a66ea3d8
Pablo Brubeck <brubeck@protonmail.com>
2025-09-04 17:15:35 +0100

    Define P1 with trivial Vandermonde matrix

--- FIAT/lagrange.py
+++ FIAT/lagrange.py
@@ -83,7 +83,6 @@ class Lagrange(finite_element.CiarletElement):
             points = get_lagrange_points(dual)
             poly_set = LagrangePolynomialSet(ref_el, points)
         else:
-            poly_variant = "bubble" if ref_el.is_macrocell() else None
-            poly_set = polynomial_set.ONPolynomialSet(ref_el, degree, variant=poly_variant)
+            poly_set = polynomial_set.ONPolynomialSet(ref_el, degree, variant="bubble", scale=1)
         formdegree = 0  # 0-form
         super().__init__(poly_set, dual, degree, formdegree)
