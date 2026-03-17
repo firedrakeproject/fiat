94f69432e5eb1996bd5ec16a42d92667a319566d
Connor Ward <c.ward20@imperial.ac.uk>
2026-03-17 13:37:19 +0000

    fixup

--- finat/ufl/finiteelement.py
+++ finat/ufl/finiteelement.py
@@ -122,7 +122,6 @@ class FiniteElement(FiniteElementBase):
                                               for c in cell.sub_cells],
                                             cell=cell)
 
-
         return super().__new__(cls)
 
     def __init__(self,
