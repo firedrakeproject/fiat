a3aaa946e143ebc64ad25284c7a66e7c59539c8d
Pablo Brubeck <brubeck@protonmail.com>
2026-08-07 23:06:49 +0100

    Restore simplex lattice lowering

--- gem/interpreter.py
+++ gem/interpreter.py
@@ -8,7 +8,7 @@ from functools import singledispatch
 import itertools
 
 from gem import gem, node
-from gem.optimise import replace_delta
+from gem.optimise import replace_delta, replace_flattened
 
 __all__ = ("evaluate", )
 
@@ -401,6 +401,7 @@ def evaluate(expressions, bindings=None):
         exprs = tuple(expressions)
     except TypeError:
         exprs = (expressions, )
+    exprs = replace_flattened(exprs)
     mapper = node.Memoizer(_evaluate)
     mapper.bindings = bindings if bindings is not None else {}
     return list(map(mapper, exprs))
