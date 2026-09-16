26b6e588928fc55c1fc2e8f43308da9ae0cad4b1
Pablo Brubeck <brubeck@protonmail.com>
2026-09-16 09:41:53 +0100

    Use generalized physical basis transformations

--- finat/hct.py
+++ finat/hct.py
@@ -5,7 +5,7 @@ from gem import ListTensor
 from finat.citations import cite
 from finat.fiat_elements import ScalarFiatElement
 from finat.physically_mapped import identity, PhysicallyMappedElement
-from finat.argyris import _vertex_transform, _edge_transform, _normal_tangential_transform
+from finat.argyris import _vertex_transform, _edge_transform
 from copy import deepcopy
 
 
@@ -18,6 +18,9 @@ class HsiehCloughTocher(PhysicallyMappedElement, ScalarFiatElement):
         super().__init__(FIAT.HsiehCloughTocher(cell, degree))
 
     def basis_transformation(self, coordinate_mapping):
+        if self.degree == 3:
+            return super().basis_transformation(coordinate_mapping)
+
         V = identity(self.space_dimension())
 
         sd = self.cell.get_dimension()
@@ -36,6 +39,9 @@ class HsiehCloughTocher(PhysicallyMappedElement, ScalarFiatElement):
             V[:, s:s+sd] *= 1/h[v]
         return ListTensor(V.T)
 
+    def dof_scale(self, node, dim, havg):
+        return super().dof_scale(node, dim, havg) if dim == 0 else None
+
 
 class ReducedHsiehCloughTocher(PhysicallyMappedElement, ScalarFiatElement):
     def __init__(self, cell, degree=3):
@@ -47,43 +53,6 @@ class ReducedHsiehCloughTocher(PhysicallyMappedElement, ScalarFiatElement):
         for entity in reduced_dofs[sd-1]:
             reduced_dofs[sd-1][entity] = []
         self._entity_dofs = reduced_dofs
-
-    def basis_transformation(self, coordinate_mapping):
-        sd = self.cell.get_spatial_dimension()
-        top = self.cell.get_topology()
-        numbf = self._element.space_dimension()
-        ndof = self.space_dimension()
-        # rectangular to toss out the constraint dofs
-        V = identity(numbf, ndof)
-
-        vorder = 1
-        voffset = comb(sd + vorder, vorder)
-        _vertex_transform(V, vorder, self.cell, coordinate_mapping)
-
-        # Jacobian at barycenter
-        bary, = self.cell.make_points(sd, 0, sd+1)
-        J = coordinate_mapping.jacobian_at(bary)
-        detJ = coordinate_mapping.detJ_at(bary)
-        for e in sorted(top[1]):
-            s = len(top[0]) * voffset + e
-            v0id, v1id = (v * voffset for v in top[1][e])
-            Bnn, Bnt, Jt = _normal_tangential_transform(self.cell, J, detJ, e)
-
-            # vertex points
-            V[s, v0id] = 1/5 * Bnt
-            V[s, v1id] = -V[s, v0id]
-
-            # vertex derivatives
-            for i in range(sd):
-                V[s, v1id+1+i] = 1/10 * Bnt * Jt[i]
-                V[s, v0id+1+i] = V[s, v1id+1+i]
-
-        # Patch up conditioning
-        h = coordinate_mapping.cell_size()
-        for v in sorted(top[0]):
-            s = voffset * v + 1
-            V[:, s:s+sd] *= 1/h[v]
-        return ListTensor(V.T)
 
     # This wipes out the edge dofs.  FIAT gives a 12 DOF element
     # because we need some extra functions to help with transforming
--- finat/powell_sabin.py
+++ finat/powell_sabin.py
@@ -1,7 +1,6 @@
 import FIAT
 from gem import ListTensor
 
-from finat.argyris import _edge_transform
 from finat.citations import cite
 from finat.fiat_elements import ScalarFiatElement
 from finat.physically_mapped import identity, PhysicallyMappedElement
@@ -39,25 +38,5 @@ class QuadraticPowellSabin12(PhysicallyMappedElement, ScalarFiatElement):
         cite("PowellSabin1977")
         super().__init__(FIAT.QuadraticPowellSabin12(cell))
 
-    def basis_transformation(self, coordinate_mapping):
-        J = coordinate_mapping.jacobian_at([1/3, 1/3])
-
-        V = identity(self.space_dimension())
-
-        sd = self.cell.get_dimension()
-        top = self.cell.get_topology()
-        voffset = sd + 1
-        for v in sorted(top[0]):
-            s = voffset * v
-            for i in range(sd):
-                for j in range(sd):
-                    V[s+1+i, s+1+j] = J[j, i]
-
-        _edge_transform(V, 1, 0, self.cell, coordinate_mapping, avg=self.avg)
-
-        # Patch up conditioning
-        h = coordinate_mapping.cell_size()
-        for v in sorted(top[0]):
-            for k in range(sd):
-                V[:, voffset*v+1+k] /= h[v]
-        return ListTensor(V.T)
+    def dof_scale(self, node, dim, havg):
+        return super().dof_scale(node, dim, havg) if dim == 0 else None
