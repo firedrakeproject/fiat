76f9f3e96d74d9e12bb3c43cc90e0e72a5c82ee0
Pablo Brubeck <brubeck@protonmail.com>
2026-09-22 16:21:06 +0100

    cleanup

--- FIAT/hierarchical.py
+++ FIAT/hierarchical.py
@@ -9,7 +9,6 @@
 import numpy
 
 from FIAT import finite_element, dual_set, functional
-from FIAT.expansions import morton_index2, morton_index3
 from FIAT.reference_element import symmetric_simplex
 from FIAT.quadrature import FacetQuadratureRule
 from FIAT.polynomial_set import ONPolynomialSet
@@ -33,18 +32,7 @@ def make_dual_bubbles(ref_el, degree, interpolant_deg=None, quad_scheme=None):
     dual_degree = degree - dim - 1
     poly_set = ONPolynomialSet(ref_el, dual_degree, scale="orthonormal", variant="dual")
     dual_values = poly_set.tabulate(Q.get_points())[(0,) * dim]
-    if dim == 1:
-        dual_indices = range(degree - 1)
-    elif dim == 2:
-        dual_indices = [morton_index2(i - 2, j - 1)
-                        for j in range(1, degree + 1)
-                        for i in range(2, degree - j + 1)]
-    else:
-        dual_indices = [morton_index3(i - 2, j - 1, k - 1)
-                        for k in range(1, degree + 1)
-                        for j in range(1, degree - k + 1)
-                        for i in range(2, degree - j - k + 1)]
-    return Q, dual_values[dual_indices]
+    return Q, dual_values
 
 
 class LegendreDual(dual_set.DualSet):
@@ -96,34 +84,18 @@ class IntegratedLegendreDual(dual_set.DualSet):
     def __init__(self, ref_el, degree, interpolant_deg=None, quad_scheme=None):
         top = ref_el.get_topology()
         entity_ids = {dim: {entity: [] for entity in top[dim]} for dim in top}
-        if ref_el.is_macrocell():
-            entity_order = {entity_dim: sorted(entities)
-                            for entity_dim, entities in top.items()}
-        else:
-            spatial_dim = ref_el.get_spatial_dimension()
-            ref_top = ref_el.construct_subelement(spatial_dim).get_topology()
-            entity_order = {}
-            for entity_dim, entities in top.items():
-                entities_by_vertices = {
-                    frozenset(vertices): entity for entity, vertices in entities.items()
-                }
-                entity_order[entity_dim] = [
-                    entities_by_vertices[frozenset(vertices)]
-                    for _, vertices in sorted(ref_top[entity_dim].items())
-                ]
-
         nodes = []
-
         for dim in sorted(top):
             if degree <= dim:
                 continue
-            Q_ref, phis = make_dual_bubbles(symmetric_simplex(dim), degree,
+            sub_el = symmetric_simplex(dim)
+            Q_ref, phis = make_dual_bubbles(sub_el, degree,
                                             interpolant_deg=interpolant_deg,
                                             quad_scheme=quad_scheme)
-            for entity in entity_order[dim]:
+            for entity in sorted(top[dim]):
+                cur = len(nodes)
                 Q = FacetQuadratureRule(ref_el, dim, entity, Q_ref, avg=True)
-                dofs = list(range(len(nodes), len(nodes) + len(phis)))
-                entity_ids[dim][entity] = dofs
+                entity_ids[dim][entity].extend(range(cur, cur + len(phis)))
                 nodes.extend(functional.IntegralMoment(ref_el, Q, phi) for phi in phis)
 
         super().__init__(nodes, ref_el, entity_ids)
